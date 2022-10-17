5bb882d0c53d163bb32fee16da73ed5114c80350
agosfasanelli <agosfasanelli@gmail.com>
2022-10-17 10:59:11 +0000

    proyecto ok

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function(whoaleatorio, whatleatorio, whenaleatorio) {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = document.querySelector("#excuse");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var whoaleatorio = who[Math.floor(Math.random() * who.length)];
+  console.log(whoaleatorio);
+
+  var whatleatorio = what[Math.floor(Math.random() * what.length)];
+  console.log(whatleatorio);
+
+  var whenaleatorio = when[Math.floor(Math.random() * when.length)];
+  console.log(whenaleatorio);
+
+  excuse.innerHTML = `Sory, but ${whoaleatorio} ${whatleatorio} my homework ${whenaleatorio}`;
 };
