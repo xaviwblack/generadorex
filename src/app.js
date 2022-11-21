c4da179d8cf92dc58c3bb5f474c5edac77bbcaf5
xaviwblack <xavierwblack@gmail.com>
2022-11-21 17:37:56 +0000

    comentario

--- src/app.js
+++ src/app.js
@@ -5,27 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function(whoaleatorio, whatleatorio, whenaleatorio) {
-  //write your code here
-  const excuse = document.querySelector("#excuse");
-  let who = ["the dog", "my granma", "his turtle", "my bird"];
-  let what = ["eat", "pissed", "crushed", "broked"];
-  let when = [
-    "before the class",
-    "right in time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+window.onload = function() {
+  function generate() {
+    let who = ["the dog", "my granma", "his turtle", "my bird"];
+    let what = ["eat", "pissed", "crushed", "broked"];
+    let when = [
+      "before the class",
+      "right in time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let excuse =
+      who[Math.floor(Math.random() * who.length)] +
+      " " +
+      what[Math.floor(Math.random() * what.length)] +
+      " " +
+      when[Math.floor(Math.random() * when.length)];
+    return excuse;
+  }
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generate();
+  });
 
-  var whoaleatorio = who[Math.floor(Math.random() * who.length)];
-  console.log(whoaleatorio);
-
-  var whatleatorio = what[Math.floor(Math.random() * what.length)];
-  console.log(whatleatorio);
-
-  var whenaleatorio = when[Math.floor(Math.random() * when.length)];
-  console.log(whenaleatorio);
-
-  excuse.innerHTML = `Sory, but ${whoaleatorio} ${whatleatorio} my homework ${whenaleatorio}`;
+  console.log("Hello Rigo from the console!");
 };
